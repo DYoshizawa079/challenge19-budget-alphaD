3116a1c2c544b4155f92da487399d5674dd37ed4
DYoshizawa079 <daniely079@gmail.com>
2022-03-06 00:48:55 -0500

    Added webpack bundle JS

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,8 @@ const FILES_TO_CACHE = [
     "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
     "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
+    "./icons/icon-512x512.png",
+    "./dist/main.bundle.js"
 ];
 
 self.addEventListener('install', function(e) {
